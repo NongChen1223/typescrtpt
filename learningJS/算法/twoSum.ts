495e64d386aec11468819c8ce6f8fc0c9b8c8913
NongChen <2584533077@qq.com>
2022-07-03 17:15:43 +0800

    忽略node_modules

--- learningJS/算法/twoSum.ts
+++ learningJS/算法/twoSum.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: NongChen 2584533077@qq.com
+ * @Date: 2022-07-03 15:21:40
+ * @LastEditors: NongChen 2584533077@qq.com
+ * @LastEditTime: 2022-07-03 17:06:18
+ * @FilePath: \typescrtpt\learningJS\算法\twoSum.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /**
  * @description: 两数之和 for循环暴力
  * @event:
@@ -18,4 +26,5 @@ function twoSum(nums: number[], target: number): number[] {
   return [-1, -1];
 }
 const nums: number[] = [2, 7, 5, 11, 8, 6, 22];
-console.log(twoSum(nums, 9));
+console.log(twoSum(nums,9))
+
